2ac27a2741a5faa9d4bf2c532a0d1387ed063c91
Gino Mangnoesing <gino@grible.co>
2021-02-11 00:11:29 +0100

    generate vue app + add weather widget template + styles (rough)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WeatherWidget msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WeatherWidget from './components/WeatherWidget.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WeatherWidget
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100vh;
+  background: #1c2541;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/WeatherWidget.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="self">
+    <section class="left">
+      <header>
+        <p class="day">Wednesday</p>
+        <time datetime="2021-02-10">Feb / 10 / 2021</time>
+        <p class="city">
+        <map-pin-icon size="0.8x" class="custom-class"></map-pin-icon>
+        Kralingen, NL</p>
+      </header>
+      
+      <footer>
+        <cloud-icon size="5x" class="custom-class"></cloud-icon>
+        <p class="temperature">-4°C</p>
+        <p class="description">Scattered clouds</p>
+      </footer>
+
+    </section>
+    <section class="right"></section>
+  </div>
+</template>
+
+<script>
+import { MapPinIcon, CloudIcon } from 'vue-feather-icons'
+
+export default {
+  name: 'WeatherWidget',
+  props: {
+    msg: String
+  },
+  components: {
+    MapPinIcon,
+    CloudIcon
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.self {
+  background: #0b132b;
+  width: 800px;
+  height: 450px;
+  border-radius: 25px;
+  color: white;
+}
+.left {
+  position: relative;
+  top: -1.3em;
+  background: #5BC0BE;
+  width: 350px;
+  height: 450px;
+  padding: 1.3em 2em;
+  border-radius: 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.left .day {
+  font-weight: bold;
+  font-size: 18pt;
+  margin: 0
+}
+.left .temperature {
+  font-weight: 900;
+  font-size: 48pt;
+  margin: 0;
+}
+.left .description {
+  position: relative;
+  font-weight: 600;
+  font-size: 16pt;
+  margin: 0;
+  top: -5pt;
+}
+
+</style>
